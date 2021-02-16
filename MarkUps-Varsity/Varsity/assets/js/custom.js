7815b032b7094f3370cf7fa73bbccdc5f857e924
KG <KG>
2021-02-16 20:41:30 +0530

    major home fixes

--- MarkUps-Varsity/Varsity/assets/js/custom.js
+++ MarkUps-Varsity/Varsity/assets/js/custom.js
@@ -45,19 +45,19 @@ jQuery(function($){
 /*  2. ABOUT US VIDEO
 /* ----------------------------------------------------------- */
     // WHEN CLICK PLAY BUTTON 
-    jQuery('#mu-abtus-video').on('click', function(event) {
-      event.preventDefault();
-      $('body').append("<div id='about-video-popup'><span id='mu-video-close' class='fa fa-close'></span><iframe id='mutube-video' name='mutube-video' frameborder='0' allowfullscreen></iframe></div>");        
-      $("#mutube-video").attr("src", $(this).attr("href"));
-    });         
-    // WHEN CLICK CLOSE BUTTON
-    $(document).on('click','#mu-video-close', function(event) {     
-      $(this).parent("div").fadeOut(1000);
-    });
-    // WHEN CLICK OVERLAY BACKGROUND
-    $(document).on('click','#about-video-popup', function(event) {
-      $(this).remove();
-    });
+    // jQuery('#mu-abtus-video').on('click', function(event) {
+    //   event.preventDefault();
+    //   $('body').append("<div id='about-video-popup'><span id='mu-video-close' class='fa fa-close'></span><iframe id='mutube-video' name='mutube-video' frameborder='0' allowfullscreen></iframe></div>");        
+    //   $("#mutube-video").attr("src", $(this).attr("href"));
+    // });         
+    // // WHEN CLICK CLOSE BUTTON
+    // $(document).on('click','#mu-video-close', function(event) {     
+    //   $(this).parent("div").fadeOut(1000);
+    // });
+    // // WHEN CLICK OVERLAY BACKGROUND
+    // $(document).on('click','#about-video-popup', function(event) {
+    //   $(this).remove();
+    // });
   
   /* ----------------------------------------------------------- */
   /*  3. TOP SLIDER (SLICK SLIDER)
